3c68aaa8ec338216bb0159a7021388bbee29a12b
jiyunbb <jiyoon3000@gmail.com>
2024-04-05 23:15:56 +0900

    실행 쿼리 예시 작성 및 data.sql 추가

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # CX-toss-jiyun
 toss CX 과제
+
+python version: 3.11.8
+
+### 참고
+- 혹시 데이터 입력이 번거로우시다면 같은 디렉토리 내 data.sql을 참고 부탁드립니다.
---
+++ data.sql
@@ -0,0 +1,31 @@
+INSERT INTO user (name, created_at, modified_at) VALUES ('왕지', datetime('now'), datetime('now'));
+
+INSERT INTO certificate (status, created_at, modified_at, user_id) VALUES ('CREATED', datetime('now'), datetime('now'), 1);
+INSERT INTO certificate (status, created_at, modified_at, user_id) VALUES ('CREATED', datetime('now'), datetime('now'), 1);
+INSERT INTO certificate (status, created_at, modified_at, user_id) VALUES ('CREATED', datetime('now'), datetime('now'), 1);
+INSERT INTO certificate (status, created_at, modified_at, user_id) VALUES ('CREATED', datetime('now'), datetime('now'), 1);
+INSERT INTO certificate (status, created_at, modified_at, user_id) VALUES ('CREATED', datetime('now'), datetime('now'), 1);
+INSERT INTO certificate (status, created_at, modified_at, user_id) VALUES ('CREATED', datetime('now'), datetime('now'), 1);
+INSERT INTO certificate (status, created_at, modified_at, user_id) VALUES ('CREATED', datetime('now'), datetime('now'), 1);
+INSERT INTO certificate (status, created_at, modified_at, user_id) VALUES ('CREATED', datetime('now'), datetime('now'), 1);
+
+INSERT INTO certificate_transfer_map (created_at, modified_at, certificate_id, transfer_id) VALUES (datetime('now'), datetime('now'), 1, 1);
+INSERT INTO certificate_transfer_map (created_at, modified_at, certificate_id, transfer_id) VALUES (datetime('now'), datetime('now'), 1, 3);
+INSERT INTO certificate_transfer_map (created_at, modified_at, certificate_id, transfer_id) VALUES (datetime('now'), datetime('now'), 2, 1);
+INSERT INTO certificate_transfer_map (created_at, modified_at, certificate_id, transfer_id) VALUES (datetime('now'), datetime('now'), 2, 2);
+INSERT INTO certificate_transfer_map (created_at, modified_at, certificate_id, transfer_id) VALUES (datetime('now'), datetime('now'), 2, 3);
+INSERT INTO certificate_transfer_map (created_at, modified_at, certificate_id, transfer_id) VALUES (datetime('now'), datetime('now'), 3, 1);
+INSERT INTO certificate_transfer_map (created_at, modified_at, certificate_id, transfer_id) VALUES (datetime('now'), datetime('now'), 4, 1);
+INSERT INTO certificate_transfer_map (created_at, modified_at, certificate_id, transfer_id) VALUES (datetime('now'), datetime('now'), 4, 3);
+INSERT INTO certificate_transfer_map (created_at, modified_at, certificate_id, transfer_id) VALUES (datetime('now'), datetime('now'), 5, 1);
+INSERT INTO certificate_transfer_map (created_at, modified_at, certificate_id, transfer_id) VALUES (datetime('now'), datetime('now'), 5, 3);
+INSERT INTO certificate_transfer_map (created_at, modified_at, certificate_id, transfer_id) VALUES (datetime('now'), datetime('now'), 6, 1);
+INSERT INTO certificate_transfer_map (created_at, modified_at, certificate_id, transfer_id) VALUES (datetime('now'), datetime('now'), 6, 3);
+INSERT INTO certificate_transfer_map (created_at, modified_at, certificate_id, transfer_id) VALUES (datetime('now'), datetime('now'), 7, 1);
+INSERT INTO certificate_transfer_map (created_at, modified_at, certificate_id, transfer_id) VALUES (datetime('now'), datetime('now'), 7, 3);
+INSERT INTO certificate_transfer_map (created_at, modified_at, certificate_id, transfer_id) VALUES (datetime('now'), datetime('now'), 8, 1);
+INSERT INTO certificate_transfer_map (created_at, modified_at, certificate_id, transfer_id) VALUES (datetime('now'), datetime('now'), 8, 3);
+
+INSERT INTO transfer (created_at, modified_at, user_id) VALUES (datetime('now'), datetime('now'), 1);
+INSERT INTO transfer (created_at, modified_at, user_id) VALUES (datetime('now'), datetime('now'), 1);
+INSERT INTO transfer (created_at, modified_at, user_id) VALUES (datetime('now'), datetime('now'), 1);
