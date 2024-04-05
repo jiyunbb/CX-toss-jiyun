89a13b69a93820e3ac3aeae0d7597970653e227a
jiyunbb <jiyoon3000@gmail.com>
2024-04-05 23:02:40 +0900

    세팅 파일 추가 - settings, urls, requirements.txt 파일 추가

--- django_project/settings.py
+++ django_project/settings.py
@@ -5,11 +5,33 @@ SECRET_KEY = "F645B5810A118A3C8A2C68C720F31084388B586B549E4667D6000B74BB007D36"
 DEBUG = True
 ALLOWED_HOSTS = []
 INSTALLED_APPS = [
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
     "apps.core",
+    "apps.certificates",
 ]
 MIDDLEWARE = []
 ROOT_URLCONF = "django_project.urls"
-TEMPLATES = []
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [f'{BASE_DIR}/templates'],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
 WSGI_APPLICATION = "django_project.wsgi.application"
 DATABASES = {
     "default": {
@@ -24,3 +46,21 @@ USE_I18N = True
 USE_TZ = False
 STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django.db.backends': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+    },
+}
--- django_project/urls.py
+++ django_project/urls.py
@@ -1,6 +1,12 @@
+from django.urls import path, include
+
+from apps.certificates import views as certificates_views
 from apps.core import views as core_views
-from django.urls import path
 
 urlpatterns = [
     path("health/", core_views.HealthAPI.as_view()),
+    path("v1/", include([
+        path("api/certificates/", certificates_views.CertificateListCreateView.as_view()),
+        path("api/certificates/<int:certificate_id>/", certificates_views.CertificateHTML.as_view())
+    ])),
 ]
